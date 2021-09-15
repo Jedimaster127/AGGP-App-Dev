a1c5688a776cae21f5e03d041aacd0e1327fd670
Kai G <jedipokemon127@yahoo.com>
2021-09-14 20:10:40 -0400

    Assignment 1

---
+++ Assignment 1/Assets/scripts/joystickplayer.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class joystickplayer : MonoBehaviour
+{
+    public float speed;
+    public VariableJoystick variableJoystick;
+    public Rigidbody2D rb;
+
+    public void FixedUpdate()
+    {
+        Vector3 direction = Vector3.forward * variableJoystick.Vertical + Vector3.right * variableJoystick.Horizontal;
+        rb.AddForce(direction * speed * Time.fixedDeltaTime, (ForceMode2D)ForceMode.VelocityChange);
+    }
+}
